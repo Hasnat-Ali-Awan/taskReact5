316ca7674b6a9e29a559b73eb1853af33d4b7c3a
unknown <hasnataliawan088@gmail.com>
2026-07-20 15:32:40 +0500

    Wrap input field in a div for better structure

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,11 +1,13 @@
 function InputField({ text, setText }) {
     return (
-        <input
-            type="text"
-            placeholder="Enter your name..."
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-        />
+        <div>
+            <input
+                type="text"
+                placeholder="Enter your name..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+        </div>
     );
 }
 
